a8e0f6556917ec6d69ca4ec8d9e423768bec38ce
Christopher Beards <BeardsCM@gmail.com>
2018-10-23 15:07:35 -0400

    Initial styling implemented, site runs

--- index.php
+++ index.php
@@ -7,13 +7,26 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   <title>PHP Crud</title>
 </head>
+
 <body>
+  <div class="justify-content-center">
   <form action="" method="POST">
-    <label>Name</label>
-    <input type="text" name="name" value="Enter your name">
-    <label>Location</label>
-    <input type="text" name="location" value="Enter your location">
-    <button type="submit" name="save">Save</button>
+
+    <div class="form-group">
+      <label>Name</label>
+      <input type="text" name="name" class="form-control" placeholder="Enter your name">
+    </div>
+
+    <div class="form-group">
+     <label>Location</label>
+      <input type="text" name="location" class="form-control" placeholder="Enter your location">
+    </div>
+
+    <div class="form-group">
+      <button type="submit" class="btn btn-primary" name="save">Save</button>
+    </div>
+
   </form>
+  </div>
 </body>
 </html>
