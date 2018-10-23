78da51b6577206e52d6b01f28d93e0fb60475ddf
Christopher Beards <BeardsCM@gmail.com>
2018-10-23 15:30:23 -0400

    saves name to database

--- process.php
+++ process.php
@@ -3,3 +3,13 @@
   # Object Oriented Style
   # Connecting to the DB
   $mysqli = new mysqli('localhost', 'root', '', 'crud') or die(mysqli_error($mysqli));
+
+  # Check if `save` has been pressed
+  if (isset($_POST['save'])) {
+    # Store the name and location in variables
+    $name = $_POST['name'];
+    $location = $_POST['location'];
+
+    # Insert records into the DB
+    $mysqli->query("INSERT INTO data (name, location) VALUES('$name', '$location')") or die($mysqli->error);
+  }
