98b6f99553c769e1f2e10d5f89a1f542a85eb057
Christopher Beards <BeardsCM@gmail.com>
2018-10-26 15:24:18 -0400

    Math.php; basic math options in php

---
+++ exercises/math.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <title>PHP: Math</title>
+</head>
+<body>
+<div class="jumbotron">
+  <div class="container" style="text-align: center">
+    <h1>Operations</h1>
+    <p>
+      <?php 
+        // Addition
+        echo 1 + 1;
+        echo "<br>";
+        // Subtraction
+        echo 1 - 1;
+        echo "<br>";
+        // Division
+        echo 1 / 1;
+        echo "<br>";
+        // Multiplication
+        echo 1 * 2;
+        echo "<br>";
+        // Modulus  
+        echo 10 % 2;
+        echo "<br>";
+      ?>
+    </p>
+  </div>
+</div>
+</body>
+</html>
