2dd8ed53d26f9086383e1085d5850cb14b82a8d6
Christopher Beards <BeardsCM@gmail.com>
2018-10-26 13:42:05 -0400

    Variables; Practice and notes added

--- exercises/variables.php
+++ exercises/variables.php
@@ -8,7 +8,31 @@
 </head>
 <body>
 
+<?php 
+  // Variables are case sensitive and are declared with a `$`
+  // Strings: use back-ticks, single, or double quotes
+  $stringName = 'Variable Data as a string';
+  
+  // Numbers: no quotes
+  $number = 100;
 
+  // With HTML
+  $h1Heading = "<h3> PHP Variable Practice </h3>";
+?>  
+
+<?php echo $h1Heading ?>
+<table style="width:50%; margin: auto;">
+  <tr style="background-color: #F0EAD6;">
+    <th>Strings</th>
+    <th>Numbers</th> 
+    <th>Data Manupulation</th> 
+  </tr>
+  <tr>
+    <td><?= $stringName ?></td>
+    <td><?php echo $number ?></td> 
+    <td><?php echo $stringName . " " . $number ?></td>
+  </tr>
+</table>
     
 </body>
 </html>
