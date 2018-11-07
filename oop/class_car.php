f1df26849b1119c420a51ff753f1af53dfa2d8c1
Christopher Beards <BeardsCM@gmail.com>
2018-11-07 13:18:04 -0500

    oop; created, test class created

---
+++ oop/class_car.php
@@ -0,0 +1,14 @@
+<?php 
+// Defining a class in PHP
+class Car {
+
+}
+
+if (class_exists("Car")) {
+  echo "This class already exists!";
+} else {
+  echo "This class doesn't exist yet!";
+}
+
+
+?>
