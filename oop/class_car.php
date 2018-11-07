c6dcad8e38bf89a90c8e2250f81bdbbf5e86519f
Christopher Beards <BeardsCM@gmail.com>
2018-11-07 13:26:41 -0500

    oop; Class method created

--- oop/class_car.php
+++ oop/class_car.php
@@ -2,6 +2,10 @@
 // Defining a class in PHP
 class Car {
 
+  function returnType() {
+    echo "Saab 93";
+  }
+
 }
 
 if (class_exists("Car")) {
@@ -10,5 +14,19 @@ if (class_exists("Car")) {
   echo "This class doesn't exist yet!";
 }
 
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Classes</title>
+</head>
+<body>
+
+<h1><?php echo  Car::returnType(); ?></h1>
+
+</body>
+</html>
