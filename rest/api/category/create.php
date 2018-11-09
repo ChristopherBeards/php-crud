9864305e2dcd77f7602ef21cae9044ef3a4d54c9
Christopher Beards <BeardsCM@gmail.com>
2018-11-09 11:25:33 -0500

    Category; create

---
+++ rest/api/category/create.php
@@ -0,0 +1,34 @@
+<?php
+
+  // Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  header('Access-Control-Allow-Methods: POST');
+  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,
+  Authorization, X-Requested-With');
+
+  include_once '../../config/Database.php';
+  include_once '../../models/Category.php';
+
+  // Instantiate DB & connect
+  $database = new Database();
+  $db = $database->connect();
+
+  // Instantiate category
+  $category = new Category($db);
+
+  // Get raw category data
+  $data = json_decode(file_get_contents("php://input"));
+
+  $category->name = $data->name;
+
+  // Create post
+  if ($category->create()) {
+    echo json_encode(
+      array('message' => 'Category Created')
+    );
+  } else {
+    echo json_encode(
+      array('message' => 'Category Not Created')
+    );
+  }
--- rest/models/Category.php
+++ rest/models/Category.php
@@ -34,5 +34,33 @@
 
       return $stmt;
     }
+
+    // Create Category
+    public function create() {
+      // Create query
+      $query = 'INSERT INTO ' . $this->table . '
+        SET 
+          name = :name';
+      
+      // Preapare statement
+      $stmt = $this->conn->prepare($query);
+
+      // Clean data
+      $this->name = htmlspecialchars(strip_tags($this->name));
+
+      // Bind data
+      $stmt->bindParam(':name', $this->name);
+
+      // Execute query
+      if ($stmt->execute()) {
+        return true;
+      }
+
+      // Print error if something goes wrong
+      printf("Error: %s.\n", $stmt->error);
+
+      return false;
+
+    }
   }
 ?>
